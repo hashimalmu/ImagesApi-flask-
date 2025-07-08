f00291227a274446fb1f66fecdab30354bb0a03f
hashimalmu <danlionking4@gmail.com>
2025-07-09 01:37:43 +0300

    Upload and edit images

--- android.py
+++ android.py
@@ -1,7 +1,48 @@
-from flask import Blueprint
+import os
+from os.path import basename
+import shutil
+from flask import Blueprint, request, current_app
+from flask.helpers import url_for
+from helpers import get_secure_filename_filepath
+from werkzeug.utils import redirect
+from PIL import Image
+from datetime import datetime
+from zipfile import ZipFile
 
 bp = Blueprint('android', __name__, url_prefix='/android')
 
+
+ICON_SIZES = [29, 40, 57, 58, 60, 80, 87, 114, 120, 180, 1024]
+
 @bp.route('/', methods=["POST"])
 def create_images():
-    pass
+    filename = request.json['filename']
+    filename, filepath = get_secure_filename_filepath(filename)
+
+    tempfolder = os.path.join(current_app.config['UPLOAD_FOLDER'], 'temp')
+    os.makedirs(tempfolder)
+
+    for size in ICON_SIZES:
+        outfile = os.path.join(tempfolder, f'{size}.png')
+        image = Image.open(filepath)
+        out = image.resize((size, size))
+        out.save(outfile, 'PNG')
+
+    now = datetime.now()
+    timestamp = str(datetime.timestamp(now)).rsplit('.')[0]
+    zipfilename = f'{timestamp}.zip'
+    zipfilepath = os.path.join(current_app.config['UPLOAD_FOLDER'], zipfilename)
+
+    with ZipFile(zipfilepath, 'w') as zipobj:
+        for foldername, subfolders, filenames, in os.walk(tempfolder):
+            for filename in filenames:
+                filepath = os.path.join(foldername, filename)
+                zipobj.write(filepath, basename(filepath))
+        shutil.rmtree(tempfolder)
+        return redirect(url_for('download_file', name=zipfilename))
+
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request, send_from_directory
 from actions import bp as actionsbp
+from filters import bp as filtersbp
+from android import bp as androidbp
 from helpers import allowed_extension, get_secure_filename_filepath
 
 UPLOAD_FOLDER = 'uploads/'
@@ -12,6 +14,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
 app.register_blueprint(actionsbp)
+app.register_blueprint(filtersbp)
+app.register_blueprint(androidbp)
 
 @app.route('/images', methods=['POST'])
 def upload_image():
@@ -25,11 +29,16 @@ def upload_image():
             return jsonify({'error': 'No file was selected'}), 400
         if not allowed_extension(file.filename):
             return jsonify({'error': 'The extension is not supported.'}), 400
-        
+
         filename, filepath = get_secure_filename_filepath(file.filename)
 
         file.save(filepath)
         return jsonify({
-                'message': 'File successfully uploaded',
-                'filename': filename
-            }), 201
+            'message': 'File successfully uploaded',
+            'filename': filename
+        }), 201
+
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], name)
